ffc5bce4bcca5dd1b88fd334365e654ce06571ed
Анастасия <47588908+kipina10@users.noreply.github.com>
2020-06-01 16:48:05 +0300

    Create task3-1.java

---
+++ 3-6/task3-1.java
@@ -0,0 +1,19 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println(solution(1,0,-1));
+        System.out.println(solution(1,0,0));
+        System.out.println(solution(1,0,1));
+    }
+    private static int solution(int a, int b, int c) {
+        int num = 0;
+        double discriminant = b*b - 4*a*c;
+        if (discriminant == 0) {
+            num = 1;
+        } else if (discriminant < 0) {
+            num = 0;
+        } else {
+            num = 2;
+        }
+        return num;
+    }
+}
