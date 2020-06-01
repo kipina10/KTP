656a13b922f49ad89c3637a0fe0b34a491b690c3
Анастасия <47588908+kipina10@users.noreply.github.com>
2020-06-01 16:59:22 +0300

    Create task3-4.java

---
+++ 3-6/task3-4.java
@@ -0,0 +1,23 @@
+public class task4{
+    public static void main(String[] args) {
+        System.out.println(flipEndChars("Cat, dog, and mouse."));
+        System.out.println( flipEndChars("ada"));
+        System.out.println( flipEndChars("Ada"));
+        System.out.println( flipEndChars("z"));
+
+
+    }
+    public static String flipEndChars(String s) {
+        if(s.length()<2)
+            return "Incompatible.";
+
+        if(s.charAt(0)==s.charAt(s.length()-1))
+            return "Two's a pair.";
+
+        char[] chars = s.toCharArray();
+        char a = chars[0];
+        chars[0] = chars[s.length()-1];
+        chars[s.length()-1] = a;
+        return String.valueOf(chars);
+    }
+}
